dd9e5816a08240e5b2cf6a2167859fb5ad8279a6
Daniel Poetzl <daniel.poetzl@diffblue.com>
2019-04-18 17:29:44 +0100

    Fix memory analyzer regression tests
    Recognise both NULL and 0 as null pointer value in regression test, and pass
    goto-gcc path to chain.sh.

--- regression/memory-analyzer/chain.sh
+++ regression/memory-analyzer/chain.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-goto_gcc="../../../build/bin/goto-gcc"
 memory_analyzer=$1
+goto_gcc=$2
 name=${*:$#}
 name=${name%.exe}
-args=${*:2:$#-2}
+args=${*:3:$#-3}
 
 $goto_gcc -g -std=c11 -o "${name}.exe" "${name}.c"
 $memory_analyzer $args "${name}.exe"
